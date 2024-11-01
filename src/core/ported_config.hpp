eb0a43d244860809e4a027c44a2e8ec37dda774f
Vivatsathorn Thitasirivit <vivatsathorn@outlook.co.th>
2024-11-01 15:06:33 +0700

    C++Config

---
+++ src/core/ported_config.hpp
@@ -0,0 +1,12 @@
+#ifndef LIB_XCORE_CORE_PORTED_CONFIG_HPP
+#define LIB_XCORE_CORE_PORTED_CONFIG_HPP
+
+#include <cstdint>
+#include <cstddef>
+
+namespace xcore {
+  using size_t    = size_t;
+  using nullptr_t = decltype(nullptr);
+}  // namespace xcore
+
+#endif  //LIB_XCORE_CORE_PORTED_CONFIG_HPP
