8dab9a2dac23c4f9147227180013f1495b28b4f7
Vivatsathorn Thitasirivit <vivatsathorn@outlook.co.th>
2024-11-13 16:04:57 +0700

    Network::NAV

---
+++ src/network/nav.hpp
@@ -0,0 +1,29 @@
+#ifndef LIB_XCORE_NETWORK_NAV_HPP
+#define LIB_XCORE_NETWORK_NAV_HPP
+
+namespace xcore::network {
+  template<auto MicrosTimeFunc>
+    class nav_t {
+    using TimeT = decltype(MicrosTimeFunc());
+
+  protected:
+    TimeT nav_ = {};
+
+  public:
+    void update_nav(const TimeT duration) {
+      const TimeT curr_time = MicrosTimeFunc();
+      const TimeT nav_exp   = curr_time + duration;
+
+      if (nav_exp > nav_)
+        nav_ = nav_exp;
+    }
+
+    void is_medium_free() {
+      const TimeT curr_time = MicrosTimeFunc();
+
+      return curr_time > nav_;
+    }
+  };
+}
+
+#endif //LIB_XCORE_NETWORK_NAV_HPP
