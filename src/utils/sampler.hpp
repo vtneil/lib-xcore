0720a6ba61f9766e7280c21a66bcd9d5d8b135d4
Vivatsathorn Thitasirivit <vivatsathorn@outlook.co.th>
2025-09-08 00:12:20 +0700

    Refactored `sampler_t`: added dynamic capacity handling and internal recount logic

--- src/utils/sampler.hpp
+++ src/utils/sampler.hpp
@@ -6,14 +6,15 @@
 
 LIB_XCORE_BEGIN_NAMESPACE
 
-template<size_t SamplingCount, typename Tp>
+template<size_t MaxCapacity, typename Tp>
 class sampler_t {
-  static_assert(SamplingCount > 0, "Number of samples must be greater than zero.");
+  static_assert(MaxCapacity > 0, "Number of max samples must be greater than zero.");
 
-  Tp     fifo_[SamplingCount]{};
+  Tp     fifo_[MaxCapacity]{};
   Tp     threshold_{};
   size_t idx_{};
   size_t size_{};
+  size_t cap_{MaxCapacity};
   size_t count_under_{};
   size_t count_over_{};
 
@@ -21,26 +22,30 @@ public:
   sampler_t() = default;
 
   void add_sample(const Tp &new_entry) {
-    if (size_ >= SamplingCount)
+    if (size_ >= cap_)
       compare_ge(fifo_[idx_], threshold_) ? --count_over_ : --count_under_;
     else
       ++size_;
     fifo_[idx_] = new_entry;
     compare_ge(new_entry, threshold_) ? ++count_over_ : ++count_under_;
-    idx_ = (idx_ + 1) % SamplingCount;
+    idx_ = (idx_ + 1) % cap_;
   }
 
   void set_threshold(const Tp &threshold) {
-    threshold_   = threshold;
-    count_under_ = 0;
-    count_over_  = 0;
-    for (size_t i = 0; i < size_; ++i)
-      compare_ge(fifo_[i], threshold_) ? ++count_over_ : ++count_under_;
+    threshold_ = threshold;
+    recount();
+  }
+
+  void set_capacity(const size_t cap) {
+    cap_  = cap_ > MaxCapacity ? MaxCapacity : cap;
+    size_ = size_ > cap_ ? cap_ : size_;
+    recount();
   }
 
   void reset() {
     idx_         = 0;
     size_        = 0;
+    cap_         = MaxCapacity;
     count_under_ = 0;
     count_over_  = 0;
   }
@@ -73,6 +78,14 @@ public:
 
   constexpr static bool compare_ge(const Tp &a, const Tp &b) {
     return a >= b;
+  }
+
+private:
+  void recount() {
+    count_under_ = 0;
+    count_over_  = 0;
+    for (size_t i = 0; i < size_; ++i)
+      compare_ge(fifo_[i], threshold_) ? ++count_over_ : ++count_under_;
   }
 };
 
